58a82caa5c1f28b2e6f1186e9c060eefdd795a5a
Carmen Diaz <karmenatwork@gmail.com>
2024-03-19 18:03:35 -0700

    Practice the OO approach
    - Create a database class to represent a dummy DB
    - Create  User, Product, and Category classes to represent dummy data

---
+++ models/database.py
@@ -0,0 +1,68 @@
+import json
+
+class User:
+    def __init__(self, id, name, email, password, admin = 0):
+        self.id = id 
+        self.name = name
+        self.email = email,
+        self.password = password
+        self.admin = admin
+
+    def is_admin(self):
+        return self.admin == True
+    
+    def email(self):
+        str(self.email)
+        
+
+    def __iter__(self):
+        return self.__dict__.items()    
+
+class Category:
+    def __init__(self, id, name, description):
+        self.id = id
+        self.name = name
+        self.description = description
+        
+class Product:
+    def __init__(self, id, name, author, description, price, category_name, category_id):
+        self.id = id
+        self.title = name
+        self.author = author
+        self.price = price
+        self.description = description
+        self.category = category_name
+        self.category_id = category_id
+
+class DummyEcommerceDB:
+    def __init__(self):
+        self.users = {}
+        self.catalogs = {}
+        self.categories = {}
+        self.products = {}
+
+    
+    def register(self, name, email, password, is_admin=0):
+        last_user = list(self.users.keys())[-1]
+        new_user_id = self.users[last_user]["id"] + 1
+        if email in self.users:
+            return False, f"User {email} already exists."
+        self.users[email] = { "id": new_user_id, "name": name, "email": email, "password": password, "admin": is_admin}
+        return True, "User registered successfully.", self.users[email]
+
+    def add_category(email, category_name, description):
+        categories = self.categories
+        category_key = "_".join(category_name.lower().split())
+        if category_key in categories:
+            return "Category already exists."
+        categories[category_key] = {"name": category_name, "description": description}
+        return True, "Category added successfully.", self.categories
+
+    # Ideally it should be in another file 
+    # def login(self, email, password):
+    #     #print("Login user with email: ", email)
+
+    #     if email in self.users and self.users[email]['password'] == password:
+    #         return self.users[email]
+    #     else:
+    #         return False
