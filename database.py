c122a514ba462b6fec6822e891b1de242883684b
Carmen Diaz <karmenatwork@gmail.com>
2024-04-08 16:16:20 -0700

    Add add_category and add_product to DummyEcommerceDB class

--- database.py
+++ database.py
@@ -28,10 +28,10 @@ class Category:
         self.description = description
         
 class Product:
-    def __init__(self, id, name, author, description, price, category_name, category_id):
+    def __init__(self, id, name, description, price, category_name, category_id, **kwargs):
         self.id = id
         self.name = name
-        self.author = author
+        self.author = kwargs.get("author", None)
         self.price = price
         self.description = description
         self.category = category_name
@@ -74,24 +74,68 @@ class DummyEcommerceDB:
         self.admin_id_counter += 1
         return True, f"User {email} added successfully", self.admins[email]
 
-      
+    def add_category(self, name, description):
+        categories = self.categories
+        category_key = "_".join(name.lower().split())
+        if category_key in categories:
+            # print(f"Category {category_name} already exists.")
+            return False, f"Category '{name}' already exists.", None
+        
+        ##  Add data to db.categories and update db.catalogs. Note n  
+        category_data = {"id": self.category_id_counter, "name": name, "description": description}
+        categories[category_key] = category_data
+        self.category_id_counter += 1
+
+        if category_key in self.catalogs:
+            self.catalogs[category_key].update(category_data)
+        else:
+            self.catalogs[category_key] = category_data
+
+        # print(f"Category '{category_name}' added successfully.")
+        return True , f"Category '{name}' added successfully.", categories[category_key]
+
+    def add_product(self, name, description, price, category, **kwargs):
+        categories = self.categories
+        if category not in self.categories:
+            # print("Invalid category.")
+            return False, f"Invalid category ({category}).", None
+        # categories[category]['products']
+        if 'products' in categories[category]:
+            for product in categories[category]['products']:
+                if name == product.name and price == product.price:
+                    return False, f"Product {name} already exists in {category}.", None
+
+        category_id = categories[category]['id']
+        item = Product(self.product_id_counter, name, description, price, category, category_id, **kwargs)
+        self.products[self.product_id_counter] = item
+        if 'products' in self.categories[category]:
+            self.categories[category]["products"].append(item)
+        else:
+            self.categories[category]["products"] = [item]
+
+        self.product_id_counter += 1
+        # print(f"Product '{name}' added to category '{category}' successfully.")
+        return True, f"Product '{name}' added to category '{category}' successfully.", item    
+   
 
 db = DummyEcommerceDB()
 # Adding just user
-db.add_user("user1@test.com", "userpass", "user", name = "Bianca")
-print(db.users["user1@test.com"].__dict__)
-db.add_user("user2@test.com", "userpass")
-print(db.users["user2@test.com"].__dict__)
-# Adding just admin
-db.add_admin("admin1@test.com", "adminpass1")
-print(db.admins["admin1@test.com"].__dict__)
-# Adding user that is an admin too
-db.add_user("admin2@test.com", "adminpass2", "admin", name = "Juan")
-print(db.admins["admin2@test.com"].__dict__)
+# db.add_user("user1@test.com", "userpass", "user", name = "Bianca")
+# print(db.users["user1@test.com"].__dict__)
+# db.add_user("user2@test.com", "userpass")
+# print(db.users["user2@test.com"].__dict__)
+# # Adding just admin
+# db.add_admin("admin1@test.com", "adminpass1")
+# print(db.admins["admin1@test.com"].__dict__)
+# # Adding user that is an admin too
+# db.add_user("admin2@test.com", "adminpass2", "admin", name = "Juan")
+# print(db.admins["admin2@test.com"].__dict__)
 
-# Already exists
-print(db.add_user("admin2@test.com", "adminpass2", "admin", name = "Juan"))
-print(db.add_admin("admin1@test.com", "adminpass1"))
+# # Already exists
+# print(db.add_user("admin2@test.com", "adminpass2", "admin", name = "Juan"))
+# print(db.add_admin("admin1@test.com", "adminpass1"))
 
-
-
+# Add category and products 
+print(db.add_category("Home Appliances", "Everything you need to make your home comfortable")[1])
+print(db.add_product("Lamp to relax", "Himalayan Pink Lamp", 19.99, 'home_appliances')[1])
+print(db.add_product("Lamp to relax", "Himalayan Pink Lamp", 19.99, 'home')[1])
